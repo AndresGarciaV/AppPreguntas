eaeac3d4335ff41566c6dbc2a9539cdb0bbdf50d
AndresG0 <andresgarciaVF@outlook.com>
2022-03-11 23:54:33 -0500

    subiendo Mi app en desarollo

--- app/src/main/java/com/example/averresponde/presentacion/PreguntasActivity.kt
+++ app/src/main/java/com/example/averresponde/presentacion/PreguntasActivity.kt
@@ -5,10 +5,15 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.TextView
 import com.example.averresponde.PuntuacionActivity
 import com.example.averresponde.R
 import com.example.averresponde.databinding.ActivityPreguntasBinding
+
+
+
+
 
 
 class PreguntasActivity : AppCompatActivity() {
@@ -16,8 +21,8 @@ class PreguntasActivity : AppCompatActivity() {
     private var numeracionn: TextView?=null
 
     private lateinit var binding: ActivityPreguntasBinding
-    var n=1
-    var numPreguntas=10
+    var n=0
+    var numPreguntas=3
     var tiempoEnSegundos= 10
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,27 +38,16 @@ class PreguntasActivity : AppCompatActivity() {
 
 
 
-
-
-        //createFragment(FragmentPreguntas()) //Lamar al framet preguntas al abrir PreguntasActivity
-
         //Condicional para ajustar el número de preguntas
         binding.butEmpe.setOnClickListener(){
             if (n<=numPreguntas) {
-                println("------------------------------------------------Pregunta numero"+n)
-                n=n+1
-                numeracionn?.text= n.toString()
-                play()
                 createFragment(FragmentPreguntas())
-            }else{
-                println("------------------------------------------------"+"Ya estan 10 preguntas pasar a la siqguiente ventana ")
+            }else  {
                 //para cambiar a la otra activity
                 var intent= Intent(this, PuntuacionActivity::class.java)
                 startActivity(intent)
             }
         }
-
-
 
         binding.otroF.setOnClickListener(){
             var fragmentTrasaction = supportFragmentManager.beginTransaction()
@@ -62,12 +56,11 @@ class PreguntasActivity : AppCompatActivity() {
             //Pintar el fregment
             fragmentTrasaction.commit()
         }
-
-
-
-
-
     }
+
+
+
+
 
     //Metodo para mostrar el fragment de preguntas, usos de apply para reducir codigo
     private  fun createFragment(fragment: Fragment) {
@@ -75,33 +68,36 @@ class PreguntasActivity : AppCompatActivity() {
             replace(binding.FrameLayout1.id, fragment)
             addToBackStack(null)
         }.commit()
-        play() //cada vez que se llama a un fragment se llama a la cuenta regresiva
-        numeracionn?.text= (n-1).toString()
+        n=n+1
+        if(n<=numPreguntas){
+            numeracionn?.text= (n).toString()
+            play() //cada vez que se llama a un fragment se llama a la cuenta regresiva
+        }else{
+            //para cambiar a la otra activity
+            intent= Intent(this, PuntuacionActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 
-
-    //codigo cronometro
-    //funcion play para ejecutar el cuando se le asigne
+    //funcion play para cuenta regresiva
+    var tiempoSegundos =tiempoEnSegundos.toLong()
+    var tiempoMilisegundos=tiempoSegundos*1000
     fun play() {
-        var tiempoSegundos =tiempoEnSegundos.toLong()
         var tiempoMilisegundos=tiempoSegundos*1000
         object : CountDownTimer(tiempoMilisegundos,1000){
             override fun onFinish() {
                 this.cancel()//termina
-                if (n<=numPreguntas) {   //para terminar las preguntas
-                    n=n+1
-                    createFragment(FragmentPreguntas())
-                }
+                createFragment(FragmentPreguntas())
+
             }
             override fun onTick(millisUntilFinished: Long) {
                 val tiempoSegundos = (millisUntilFinished /1000).toInt()+1
                 tvCuentaAtrass?.text= tiempoSegundos.toString()
             }
-
-
         }.start()//inicia
     }
-
     //
 
 
